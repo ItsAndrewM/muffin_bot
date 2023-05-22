2f500e049eb9b35e6e2e58009c25cdcb342d2a3c
Andrew McMillan <mcmillan.r.andrew@gmail.com>
2023-05-22 10:36:21 -0700

    change to 3 hr interval

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ const startFunctionWithInterval = () => {
   // Set interval to call the function every 60 seconds
   const intervalId = setInterval(() => {
     postMuffinPhoto();
-  }, 1800000);
+  }, 10800000);
 };
 
 // Start the function with interval
